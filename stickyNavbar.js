d3d20a546f6f14d97b6e9e3f8d126bd03fd6a4ed
rupiman97 <rupiman97@users.noreply.github.com>
2022-05-26 23:51:53 +0200

    vers 1.01

---
+++ stickyNavbar.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {setSticky()};
+
+var header = document.getElementById("stickyNav");
+var sticky = header.offsetTop;
+
+function setSticky(){
+    if(window.pageYOffset > sticky){
+        header.classList.add("sticky");
+    }
+    else{
+        header.classList.remove("sticky");
+    }
+}
