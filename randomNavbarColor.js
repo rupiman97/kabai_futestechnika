f84320b3df0592c3afdc929d2c316ab53f123c30
rupiman97 <rupiman97@users.noreply.github.com>
2022-06-01 22:37:50 +0200

    update

---
+++ randomNavbarColor.js
@@ -0,0 +1,14 @@
+window.onload = function(){randomNavbarColor()}
+
+const randomColors = ["Aquamarine", "DarkSeaGreen", "Khaki", "LightGreen"];
+
+function colorPicker(){
+    return Math.floor(Math.random() * 4);
+}
+
+function randomNavbarColor(){
+    let navbar = document.getElementById("stickyNav");
+    let colorValue = colorPicker();
+    navbar.style.backgroundColor = randomColors[colorValue];
+    
+}
--- startBackground.js
+++ startBackground.js
@@ -3,7 +3,6 @@ window.onload = function() {setBackgroundImageSize()};
 window.onresize = function() {setBackgroundImageSize()};
 
 function setBackgroundImageSize() {
-    console.log("beleptem");
     let screenWidth = window.innerWidth;
     let val = document.getElementById("startingDistanceID");
     val.style.marginTop = screenWidth/2.3 + "px";
