38be719aabd3b4bcab4692bce0d90d0ba7802c47
rupiman97 <rupiman97@users.noreply.github.com>
2022-12-06 19:29:36 +0100

    enlarge picture border shadow changed

--- js/pictureEnlarge.js
+++ js/pictureEnlarge.js
@@ -4,17 +4,19 @@ function enlargePicture(id){
     console.log(enlargedPicturesPresented.length);
     if(!val.classList.contains("onclickReferPicture") && enlargedPicturesPresented.length == 0){
         val.classList.add("onclickReferPicture");
+        val.classList.remove("referPicture");
         let screenWidth = window.innerWidth;
         let screenHeight = window.innerHeight;
         let leftPercent = (screenWidth - val.width)/2/screenWidth*100 + "%";
         console.log("leftPercent: " + leftPercent);
-        let bottomPercent = (screenHeight - val.height)/2/screenWidth*100 + "%";
+        let bottomPercent = (screenHeight - val.height)/2/screenHeight*100 + "%";
         console.log("bottomPercent: " + bottomPercent);
         val.style.left = leftPercent;
         val.style.bottom = bottomPercent;
     }
     else{
         val.classList.remove("onclickReferPicture");
+        val.classList.add("referPicture");
         val.style.left = 0;
     }
 };
